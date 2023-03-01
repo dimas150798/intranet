7270c98c7553e3b49a5e9873f12158cf0771d6a6
dimas150798 <47965622+dimas150798@users.noreply.github.com>
2023-03-01 22:16:29 +0700

    intranet

--- application/controllers/admin/DataBarangV2/Add_StockKeluarKabel.php
+++ application/controllers/admin/DataBarangV2/Add_StockKeluarKabel.php
@@ -20,26 +20,57 @@ class Add_StockKeluarKabel extends CI_Controller
 
     public function addStockKeluar($id)
     {
-        $checkStockBarang   = $this->BarangModelV2->checkDuplicateStockBarang($id);
-        $idStockBarang      = $checkStockBarang->id_stockBarang;
+        // $checkStockBarang   = $this->BarangModelV2->checkDuplicateStockBarang($id);
+        // $idStockBarang      = $checkStockBarang->id_stockBarang;
 
-        $checkStockRincian  = $this->BarangModelV2->checkStockRincianBarang($idStockBarang);
-        $idStockRincian     = $checkStockRincian->id_stockRincian;
+        // $checkStockRincian  = $this->BarangModelV2->checkStockRincianBarang($idStockBarang);
+        // $idStockRincian     = $checkStockRincian->id_stockRincian;
 
-        $jumlahStockRincian = $checkStockRincian->jumlah;
+        // $jumlahStockRincian = $checkStockRincian->jumlah;
 
-        // var_dump($checkStockRincian);
-        // die;
 
-        if ($idStockRincian == null) {
-            echo "
-            <script>
-            alert('Masukkan Detail Barang');history.go(-1)
-            document.location.href = 'tambahData';            
-            </script>
-            ";
-        } else {
-            $data['barangNama']  =  $this->db->query("SELECT data_stockbarang.id_stockBarang, data_stockbarang.id_barang, data_stockbarang.jumlah_stockBarang, 
+        // if ($idStockRincian == null) {
+        //     echo "
+        //     <script>
+        //     alert('Masukkan Detail Barang');history.go(-1)
+        //     document.location.href = 'tambahData';
+        //     </script>
+        //     ";
+        // } else {
+        //     $data['barangNama']  =  $this->db->query("SELECT data_stockbarang.id_stockBarang, data_stockbarang.id_barang, data_stockbarang.jumlah_stockBarang,
+        //     data_stockbarang.jumlah_stockMutasi, data_namabarang.nama_barang, data_stockrincian.jumlah
+
+        //     FROM data_stockbarang
+
+        //     LEFT JOIN data_namabarang ON data_stockbarang.id_barang = data_namabarang.id_barang
+        //     LEFT JOIN data_stockrincian ON data_stockbarang.id_stockBarang = data_stockrincian.id_stockBarang
+
+        //     WHERE data_stockbarang.id_barang = '$id'
+
+        //     GROUP BY data_stockbarang.id_stockBarang
+
+        //     ")->result();
+
+        //     $data['dataPegawai'] = $this->BarangModelV2->dataPegawai();
+        //     $data['dataStatus'] = $this->BarangModelV2->dataStatus();
+        //     $data['dataKodeKabel'] = $this->BarangModelV2->dataKodeKabel($id);
+        //     $data['dataCustomer'] = $this->BarangModelV2->dataCustomerAll();
+
+        //     $this->load->view('template/header', $data);
+        //     $this->load->view('template/sidebarAdmin', $data);
+        //     $this->load->view('admin/DataBarangV2/add_StockKeluarKabel', $data);
+        //     $this->load->view('template/footer', $data);
+        // }
+
+        // $checkStockBarang   = $this->BarangModelV2->checkDuplicateStockBarang($id);
+        // $idStockBarang      = $checkStockBarang->id_stockBarang;
+
+        // $checkStockRincian  = $this->BarangModelV2->checkStockRincianBarang($idStockBarang);
+        // $idStockRincian     = $checkStockRincian->id_stockRincian;
+
+        // $jumlahStockRincian = $checkStockRincian->jumlah;
+
+        $data['barangNama']  =  $this->db->query("SELECT data_stockbarang.id_stockBarang, data_stockbarang.id_barang, data_stockbarang.jumlah_stockBarang, 
             data_stockbarang.jumlah_stockMutasi, data_namabarang.nama_barang, data_stockrincian.jumlah
     
             FROM data_stockbarang
@@ -53,16 +84,15 @@ class Add_StockKeluarKabel extends CI_Controller
     
             ")->result();
 
-            $data['dataPegawai'] = $this->BarangModelV2->dataPegawai();
-            $data['dataStatus'] = $this->BarangModelV2->dataStatus();
-            $data['dataKodeKabel'] = $this->BarangModelV2->dataKodeKabel($id);
-            $data['dataCustomer'] = $this->BarangModelV2->dataCustomerAll();
+        $data['dataPegawai'] = $this->BarangModelV2->dataPegawai();
+        $data['dataStatus'] = $this->BarangModelV2->dataStatus();
+        $data['dataKodeKabel'] = $this->BarangModelV2->dataKodeKabel($id);
+        $data['dataCustomer'] = $this->BarangModelV2->dataCustomerAll();
 
-            $this->load->view('template/header', $data);
-            $this->load->view('template/sidebarAdmin', $data);
-            $this->load->view('admin/DataBarangV2/add_StockKeluarKabel', $data);
-            $this->load->view('template/footer', $data);
-        }
+        $this->load->view('template/header', $data);
+        $this->load->view('template/sidebarAdmin', $data);
+        $this->load->view('admin/DataBarangV2/add_StockKeluarKabel', $data);
+        $this->load->view('template/footer', $data);
     }
 
     // data keluar ATK
@@ -97,11 +127,6 @@ class Add_StockKeluarKabel extends CI_Controller
             // Stock Mutasi Stock Gudang
             $stockMutasi            = $jumlahMutasi + $jumlah;
 
-            $checkStockRincian  = $this->BarangModelV2->checkStockKodeBarang($kode_barang);
-            $jumlahStockRincian = $checkStockRincian->jumlah;
-
-            $stockRincian           = $jumlahStockRincian - $jumlah;
-
             $dataStockKeluar = array(
                 'id_stockBarang'    => $id_stockBarang,
                 'kode_barang'       => $kode_barang,
@@ -117,15 +142,6 @@ class Add_StockKeluarKabel extends CI_Controller
                 'jumlah_stockBarang'    => $stockGudang,
                 'jumlah_stockMutasi'    => $stockMutasi,
                 'tanggal_mutasi'        => $tanggal
-            );
-
-            $dataStockRincian = array(
-                'tanggal'           => $tanggal,
-                'kode_barang'       => $kode_barang,
-                'id_stockBarang'    => $id_stockBarang,
-                'jumlah'            => $stockRincian,
-                'tanggal'           => $tanggal,
-                'id_status'         => 12
             );
 
             $where = array(
@@ -144,32 +160,15 @@ class Add_StockKeluarKabel extends CI_Controller
                 </script>
                 ";
             } else {
-                if ($jumlah >= $jumlahStockRincian) {
-                    echo "
-                <script>
-                alert('Melebihi Stock Yang Ada');history.go(-1)
-                document.location.href = 'tambahData';            
-                </script>
-                ";
-                } else {
-                    if ($kode_barang == null && $kode_barang == 0) {
-                        echo "
-                        <script>
-                        alert('Stock Barang Kosong');history.go(-1)
-                        document.location.href = 'tambahData';            
-                        </script>
-                        ";
-                    } else {
-                        $this->BarangModelV2->updateData('data_stockrincian', $dataStockRincian, $whereStockRincian);
-                        $this->BarangModelV2->updateData('data_stockbarang', $dataStockGudang, $where);
-                        $this->BarangModelV2->insertData($dataStockKeluar, 'data_stockkeluar');
-                        $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-dismissible fade show" role="alert">
+
+                        // $this->BarangModelV2->updateData('data_stockrincian', $dataStockRincian, $whereStockRincian);
+                $this->BarangModelV2->updateData('data_stockbarang', $dataStockGudang, $where);
+                $this->BarangModelV2->insertData($dataStockKeluar, 'data_stockkeluar');
+                $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-dismissible fade show" role="alert">
                                             <strong>STOCK BARANG BERHASIL DIKURANGIN</strong>
                                             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                                             </div>');
-                        redirect('admin/DataBarangV2/Data_StockBarangPelanggan');
-                    }
-                }
+                redirect('admin/DataBarangV2/Data_StockBarangPelanggan');
             }
         }
     }
--- application/views/admin/DataBarangV2/add_StockKeluarKabel.php
+++ application/views/admin/DataBarangV2/add_StockKeluarKabel.php
@@ -61,95 +61,95 @@
 										placeholder="Masukkan nama..." readonly>
 								</div>
 
-								<div class="col-sm-3 mt-3">
+								<!-- <div class="col-sm-3 mt-3">
 									<label for="kodeBarang" class="form-label" style="font-weight: bold;">Kode Barang :
 										<span class="text-danger text-center">*</span></label>
 									<select name="kodeBarang" id="kodeBarang" class="form-control" required>
 										<option disabled selected>-- Pilih Kode --</option>
 										<?php foreach ($dataKodeKabel as $kabel) : ?>
-										<option
-											value="<?php echo $kabel['kode_barang']?>">
-											<?php echo $kabel['kode_barang']?>
-										</option>
-										<?php endforeach; ?>
-									</select>
-								</div>
+								<option
+									value="<?php echo $kabel['kode_barang']?>">
+									<?php echo $kabel['kode_barang']?>
+								</option>
+								<?php endforeach; ?>
+								</select>
+							</div> -->
 
-								<div class="col-sm-2 mt-3">
-									<label for="jumlah" class="form-label" style="font-weight: bold;"> Jumlah :
-										<span class="text-danger">*</span></label>
-									<input type="number" class="form-control" name="jumlah" id="jumlah"
-										value="<?php echo $data->jumlah_stockBarang?>"
-										autofocus name="jumlah" min="1"
-										max="<?php echo $data->jumlah_stockBarang?>"
-										placeholder="Masukkan jumlah...">
-									<div class="bg-danger">
-										<small
-											class="text-white"><?php echo form_error('jumlah'); ?></small>
-									</div>
-								</div>
-
-								<div class="col-sm-3 mt-3">
-									<label for="tanggal" class="form-label" style="font-weight: bold;"> Tanggal :
-										<span class="text-danger">*</span></label>
-									<input type="date" class="form-control" name="tanggal" id="tanggal" value=""
-										placeholder="">
+							<div class="col-sm-4 mt-3">
+								<label for="jumlah" class="form-label" style="font-weight: bold;"> Jumlah :
+									<span class="text-danger">*</span></label>
+								<input type="number" class="form-control" name="jumlah" id="jumlah"
+									value="<?php echo $data->jumlah_stockBarang?>"
+									autofocus name="jumlah" min="1"
+									max="<?php echo $data->jumlah_stockBarang?>"
+									placeholder="Masukkan jumlah...">
+								<div class="bg-danger">
+									<small
+										class="text-white"><?php echo form_error('jumlah'); ?></small>
 								</div>
 							</div>
 
-							<div class="row justify-content-center">
-								<div class="col-sm-4 mt-3">
-									<label for="pegawai" class="form-label" style="font-weight: bold;">Nama Pegawai :
-										<span class="text-danger text-center">*</span></label>
-									<select name="pegawai" id="pegawai" class="form-control" required>
-										<option disabled selected>-- Pilih Pegawai --</option>
-										<?php foreach ($dataPegawai as $pegawai) : ?>
-										<option
-											value="<?php echo $pegawai['id_pegawai']?>">
-											<?php echo $pegawai['nama_pegawai']?>
-										</option>
-										<?php endforeach; ?>
-									</select>
-								</div>
-
-								<div class="col-sm-4 mt-3">
-									<label for="dataCustomer" class="form-label" style="font-weight: bold;">Nama
-										Customer :
-										<span class="text-danger text-center">*</span></label>
-									<select name="dataCustomer" id="dataCustomer" class="form-control" required>
-										<option disabled selected>-- Pilih Customer --</option>
-										<?php foreach ($dataCustomer as $customer) : ?>
-										<option
-											value="<?php echo $customer['id_customer']?>">
-											<?php echo $customer['nama_customer']?>
-										</option>
-										<?php endforeach; ?>
-									</select>
-								</div>
-
-								<div class="col-sm-4 mt-3">
-									<label for="keterangan" class="form-label" style="font-weight: bold;"> Keterangan
-										:</label>
-									<input type="text" class="form-control" name="keterangan" id="keterangan" value=""
-										placeholder="Masukkan Keterangan...">
-								</div>
+							<div class="col-sm-4 mt-3">
+								<label for="tanggal" class="form-label" style="font-weight: bold;"> Tanggal :
+									<span class="text-danger">*</span></label>
+								<input type="date" class="form-control" name="tanggal" id="tanggal" value=""
+									placeholder="">
 							</div>
-
-							<div class="row mt-3">
-								<div class="col-sm-12 d-flex justify-content-end">
-									<button type="submit" class="btn btn-success mt-2 justify-content-end"
-										style="margin-right: 5px;"><i class="bi bi-plus-circle"></i> Simpan</button>
-									<a class="btn btn-danger mt-2 justify-content-end"
-										href="<?php echo base_url('admin/DataBarangV2/data_StockBarangPelanggan')?>"><i
-											class="bi bi-backspace-fill"></i> Kembali</a>
-								</div>
-							</div>
-
-						</form>
-						<?php endforeach; ?>
-
 					</div>
+
+					<div class="row justify-content-center">
+						<div class="col-sm-4 mt-3">
+							<label for="pegawai" class="form-label" style="font-weight: bold;">Nama Pegawai :
+								<span class="text-danger text-center">*</span></label>
+							<select name="pegawai" id="pegawai" class="form-control" required>
+								<option disabled selected>-- Pilih Pegawai --</option>
+								<?php foreach ($dataPegawai as $pegawai) : ?>
+								<option
+									value="<?php echo $pegawai['id_pegawai']?>">
+									<?php echo $pegawai['nama_pegawai']?>
+								</option>
+								<?php endforeach; ?>
+							</select>
+						</div>
+
+						<div class="col-sm-4 mt-3">
+							<label for="dataCustomer" class="form-label" style="font-weight: bold;">Nama
+								Customer :
+								<span class="text-danger text-center">*</span></label>
+							<select name="dataCustomer" id="dataCustomer" class="form-control" required>
+								<option disabled selected>-- Pilih Customer --</option>
+								<?php foreach ($dataCustomer as $customer) : ?>
+								<option
+									value="<?php echo $customer['id_customer']?>">
+									<?php echo $customer['nama_customer']?>
+								</option>
+								<?php endforeach; ?>
+							</select>
+						</div>
+
+						<div class="col-sm-4 mt-3">
+							<label for="keterangan" class="form-label" style="font-weight: bold;"> Keterangan
+								:</label>
+							<input type="text" class="form-control" name="keterangan" id="keterangan" value=""
+								placeholder="Masukkan Keterangan...">
+						</div>
+					</div>
+
+					<div class="row mt-3">
+						<div class="col-sm-12 d-flex justify-content-end">
+							<button type="submit" class="btn btn-success mt-2 justify-content-end"
+								style="margin-right: 5px;"><i class="bi bi-plus-circle"></i> Simpan</button>
+							<a class="btn btn-danger mt-2 justify-content-end"
+								href="<?php echo base_url('admin/DataBarangV2/data_StockBarangPelanggan')?>"><i
+									class="bi bi-backspace-fill"></i> Kembali</a>
+						</div>
+					</div>
+
+					</form>
+					<?php endforeach; ?>
+
 				</div>
 			</div>
 		</div>
-	</main>
+</div>
+</main>
